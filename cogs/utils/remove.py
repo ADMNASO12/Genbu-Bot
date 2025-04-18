f19111a05475e9416b817324660a5aed294a57ce
Reim-developer <contact.kaxtr@gmail.com>
2025-04-18 12:32:03 +0700

    Add Remove cog to manage custom word deletions in Discord

---
+++ cogs/utils/remove.py
@@ -0,0 +1,89 @@
+import discord
+import datetime
+from config_load import ConfigLoad
+from pymongo import MongoClient
+from discord.ext import commands
+
+class Remove(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+        self.uri = ConfigLoad.load_config()
+        self.client = MongoClient(self.uri)
+        
+        self.database = self.client["CustomWords"]
+        self.wordsDB = self.database["CustomWords"]
+
+    def remove_word(self, word: str) -> bool:
+        result = self.wordsDB.find_one({"wordsList.word": word})
+
+        if not result:
+            return False
+        
+        self.wordsDB.update_one(
+            {"wordsList.word": word},  
+            {"$pull": {"wordsList": {"word": word}}},  
+            upsert = False
+        )
+        return True
+
+    @commands.command()
+    @commands.has_any_role(
+        1073747375245496410, 
+        1073731640918032517,
+        1044529340207087616
+    )
+    async def remove(self, ctx: commands.Context, word: str = None) -> None:
+        if not ctx.guild:
+            return
+
+        botUser = ctx.guild.get_member(1334471950755565579)
+        
+        if word is None:
+            embed = discord.Embed(
+                description = (
+                    f"{ctx.author.name} ơi, hãy nhập từ khóa tùy chỉnh mà bạn muốn xóa\n" + \
+                    "`.remove <từ khóa>`" 
+                ),
+                timestamp = datetime.datetime.now(),
+                color = 0x2f3136
+            )
+            embed.set_thumbnail(url = botUser.avatar.url)
+            await ctx.channel.send(embed = embed)
+
+            return
+
+        if not self.remove_word(word):
+            embed = discord.Embed(
+                description = (
+                    f"{ctx.author.name} ơi, từ khóa tùy chỉnh {word} không tồn tại, " +
+                    "bạn không cần xóa từ khóa tùy chỉnh nữa"
+                ),
+                timestamp = datetime.datetime.now(),
+                color = 0x2f3136
+            )
+            embed.set_thumbnail(url = botUser.avatar.url)
+            await ctx.channel.send(embed = embed)
+            
+            return
+
+        blueDot = "<:dot:1067002963605852201>"
+        rightArrow = "<:ArrowRight:1334500913066278986>"
+
+        embed = discord.Embed(
+            color = 0x2f3136,
+            timestamp = datetime.datetime.now()
+        )
+        embed.add_field(
+            name = f"{ctx.author.name} ơi, mình đã xử lí lệnh của bạn như sau:",
+            value = (
+                f"{rightArrow} Xóa từ khóa tùy chỉnh:\n\n" +
+                f"{blueDot} {word}"
+            )
+        )
+        embed.set_thumbnail(url = botUser.avatar.url)
+        
+        await ctx.channel.send(embed = embed)
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(Remove(bot))
