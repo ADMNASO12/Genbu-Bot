f5251ac0650402fa5074b0fb299c3e8e9f0d1ac4
Reim-developer <contact.kaxtr@gmail.com>
2025-06-23 11:34:50 +0700

    Add Genbu to control flow

--- cogs/events/CustomWords.py
+++ cogs/events/CustomWords.py
@@ -9,7 +9,7 @@ from discord.ext import commands
 class CustomWords(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.log_channel = self.bot.get_channel(1374294073774440528)
+        self.log_channel = self.bot.get_channel(1386563502113820682)
         self.logging = logging.basicConfig(
             level = logging.WARNING,
             format = "[%(asctime)s] [%(levelname)s] %(message)s",
@@ -32,49 +32,51 @@ class CustomWords(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> None:
+        genbu = self.bot.get_guild(1073730331263373372)
 
-        if message.author.bot:
-            return
-        
-        if not message.guild:
-            return
+        if genbu:
+            if message.author.bot:
+                return
+            
+            if not message.guild:
+                return
 
-        msg = self.find_words(message.content)
-        if msg:
-            await message.channel.send(msg, allowed_mentions = discord.AllowedMentions.none())
-        
-        else:
-            msg = message
-            msg_text: str = msg.content
-            attachments = msg.attachments
-            time_now = int(datetime.datetime.now(datetime.timezone.utc).timestamp())
+            msg = self.find_words(message.content)
+            if msg:
+                await message.channel.send(msg, allowed_mentions = discord.AllowedMentions.none())
+            
+            else:
+                msg = message
+                msg_text: str = msg.content
+                attachments = msg.attachments
+                time_now = int(datetime.datetime.now(datetime.timezone.utc).timestamp())
 
-            match (bool(msg_text), bool(attachments)):
-                case (True, False):
-                    log_msg_text: str = f"""
-* ID người dùng: {msg.author.id}
-* Đề cập: {msg.author.mention}
-* Nội dung tin nhắn: {msg_text}
-* Phân loại nội dung: Tin nhắn văn bản
-* Gửi vào lúc: <t:{time_now}>
+                match (bool(msg_text), bool(attachments)):
+                    case (True, False):
+                        log_msg_text: str = f"""
+    * ID người dùng: {msg.author.id}
+    * Đề cập: {msg.author.mention}
+    * Nội dung tin nhắn: {msg_text}
+    * Phân loại nội dung: Tin nhắn văn bản
+    * Gửi vào lúc: <t:{time_now}>
 
-                    """
-                    await self.send_log_msg(msg = log_msg_text)
+                        """
+                        await self.send_log_msg(msg = log_msg_text)
 
-                case (False, True):
-                    log_msg_text: str = f"""
-* ID người dùng: {msg.author.id}
-* Đề cập: {msg.author.mention}
-* Nội dung tin nhắn: Hình ảnh
-* Phân loại nội dung: Tin nhắn hình ảnh
-* Gửi vào lúc: <t:{time_now}>
+                    case (False, True):
+                        log_msg_text: str = f"""
+    * ID người dùng: {msg.author.id}
+    * Đề cập: {msg.author.mention}
+    * Nội dung tin nhắn: Hình ảnh
+    * Phân loại nội dung: Tin nhắn hình ảnh
+    * Gửi vào lúc: <t:{time_now}>
 
-                    """
-                    image_url = attachments[0].url
-                    await self.send_log_msg(msg = log_msg_text, url = image_url)
-                
-                case _:
-                    pass
+                        """
+                        image_url = attachments[0].url
+                        await self.send_log_msg(msg = log_msg_text, url = image_url)
+                    
+                    case _:
+                        pass
         
     @overload
     async def send_log_msg(self, msg: str) -> None: ...
