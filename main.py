ee3c6ea2320af8811373b92451d6faaec468775f
Reim-developer <contact.kaxtr@gmail.com>
2025-06-25 05:39:56 +0700

    Add sync command for guild command synchronization

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import discord
 import json
 from discord.ext import commands
+from typing import Literal, Optional
+
+from type import Context
 
 with open("./config/config.json") as configFile:
     data = json.load(configFile)
@@ -22,6 +25,7 @@ cogList = [
     "cogs.utils.add",
     "cogs.utils.remove",
     "cogs.utils.cfs",
+    "cogs.utils.embed",
     "cogs.events.MsgDel",
     "cogs.events.MsgEdit",
     "cogs.utils.game_ping"
@@ -32,10 +36,49 @@ async def setupCogs() -> None:
         await bot.load_extension(cog)
         print(f"Loaded {cog} successfully")
 
+@bot.command()
+@commands.guild_only()
+@commands.is_owner()
+async def sync(ctx: Context, guilds: commands.Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+    if not guilds:
+        if spec == "~":
+            synced = await ctx.bot.tree.sync(guild = ctx.guild)
+        elif spec == "*":
+            if ctx.guild:
+                ctx.bot.tree.copy_global_to(guild = ctx.guild)
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "^":
+            ctx.bot.tree.clear_commands(guild = None)
+            await ctx.bot.tree.sync(guild=ctx.guild)
+            synced = []
+        else:
+            synced = await ctx.bot.tree.sync()
+
+        await ctx.send(
+            f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
+        )
+        return
+
+    ret = 0
+    for guild in guilds:
+        try:
+            await ctx.bot.tree.sync(guild=guild)
+        except discord.HTTPException:
+            pass
+        else:
+            ret += 1
+
+    await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
+
 @bot.event
 async def on_ready() -> None:
     await setupCogs()
+    #bot.tree.clear_commands(guild = None)
     await bot.tree.sync()
+
+    for cmd in bot.tree.walk_commands():
+        print(f"Load slash: {cmd.name}")
+
     print(f"Online as {bot.user}")
 
 if(__name__ == "__main__"):
